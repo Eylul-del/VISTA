8ae45ed7d5d63a5e7894ede22bc10b8afcd196cd
Eylul-del <seymaelali090@gmail.com>
2025-08-04 21:57:49 +0300

    .

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 document.querySelectorAll(".like").forEach(function(likeDiv) {
   likeDiv.addEventListener("click", function() {
-    const heartIcon = this.querySelector("i");
+    let heartIcon = this.querySelector("i");
     heartIcon.classList.toggle("red-heart");
   });
 });
 
 
 
-
 window.showNavList = function () {
-  const sidebar = document.getElementById("sideBar");
+  let sidebar = document.getElementById("sideBar");
   if (sidebar) sidebar.classList.toggle("active");
 };
 
 window.hideBar = function () {
-  const sidebar = document.getElementById("sideBar");
+  let sidebar = document.getElementById("sideBar");
   if (sidebar) sidebar.classList.remove("active");
 };
